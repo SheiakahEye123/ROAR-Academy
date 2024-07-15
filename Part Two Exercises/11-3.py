45b80e4c3658c939916af4c54546b26841359d35
kaidenAshvini <kaidenashvini@gmail.com>
2024-07-15 02:16:41 -0700

    wk2day1 first

--- Part Two Exercises/11-3.py
+++ Part Two Exercises/11-3.py
@@ -1,5 +1,7 @@
 import PyPDF2
 import os
+import string
+import re
 
 try:
     source = "Sense-and-Sensibility-by-Jane-Austen.pdf"
@@ -21,14 +23,36 @@ for i in range(page_number):
 
     lines = page_text.split('\n')
     
-    for s in lines[3:]:
-        for word in s.split(" "):
-            if not word[-1].isalpha
+    for s in lines[3:-1]:
+        for word in re.split(" |--|-",s):
+            # if "--" in word:    
+            #     for w in word.split("--"):
+            #         t = w.strip(string.punctuation).lower()
+            #         if t not in dictionary:
+            #             dictionary[t] = 1
+            #         else:
+            #             dictionary[t] += 1
+            # else:
+            t = word.strip(string.punctuation).lower()
+            if t.isdecimal() or not t.isalnum():
+                continue
+            if t not in dictionary:
+                dictionary[t] = 1
+            else:
+                dictionary[t] += 1
 
-            sheikaheye123
 
 
-    if i == 0:
-        print(lines[3:])
+source = "freq.txt"
+print(os.path.dirname(os.path.abspath(__file__)))
+try:
+    path = os.path.dirname(os.path.abspath(__file__))
+    print(path)
+    handle = open(path+"/"+source, "wt")
+    for keys in dictionary:
+        handle.write(str(keys) + " : " + str(dictionary[keys]))
+        handle.write("\n")
 
-
+except:
+    print("Wrong!")
+    exit
